6933ba7ec3afd925b9827273d26586cacd6381ec
André de Albuquerque <itstheandre@gmail.com>
2021-11-13 10:33:15 +0100

    Push with process env situation fixed

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,3 +1,4 @@
+const a = 1;
 export const ACCESS_TOKEN_LOCAL_STORAGE_KEY = "accessToken";
 
 export function getAccessToken() {
@@ -12,4 +13,5 @@ export function removeAccessToken() {
   return localStorage.removeItem(ACCESS_TOKEN_LOCAL_STORAGE_KEY);
 }
 
-export const SERVER_URL = "http://localhost:5005";
+export const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL || "http://localhost:5005";
